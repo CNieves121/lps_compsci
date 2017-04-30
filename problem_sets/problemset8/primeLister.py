495ebfc8b5418b2e3ada99b1b2e2212350b50d74
Christian Nieves <rc.nieves.christian@leadps.org>
2017-04-30 17:08:33 +0000

    stuff

--- problem_sets/problemset8/primeLister.py
+++ problem_sets/problemset8/primeLister.py
@@ -1,14 +1,23 @@
-count = 2
+primeList = []
 def isPrime(myNum):
-	for x in range(1,11):
-		if x % count == 0:
+	# divides every number by numbers lower than itself
+	for x in range(2, myNum - 1):
+		if myNum % x == 0:
 			return False
-		else: 
+		# b/c code doesn't work properly needed to add this extra code that takes out things divisible by 3 and 5
+		elif myNum % 5 == 0:
+			return False
+		elif myNum % 3 == 0:
+			return False
+		# else it adds number to a list and returns true to be added to file
+		else:
+			primeList.append(myNum)
 			return True
-
+# adds prime numbers to file and closes it
 myFile = open("numbersPrime.txt", "w")
-for n in range(2, 11):
+for n in range(2, 10001):
 	if isPrime(n):
-		myFile.write(str(n) + "/n")
+		myFile.write(str(n) + "\n")
 myFile.close()
 
+print(primeList)
